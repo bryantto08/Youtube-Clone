fe4082adf74c61f176189a46b8a1171e6f58d575
bryantto08 <bryantto08@gmail.com>
2023-10-03 15:58:00 -0400

    Successfully Processed Videos Locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,38 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+
+app.use(express.json());  // middleware so that express can handle JSON
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.post('/process-video', (req, res) => {
+    // Get path of the input video file that we want to convert from the request body
+    // We want to convert it to 360p and then save it
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    // error handling to see if body data is undefined
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")  // convert to 360p resolution
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully");
+        })
+        .on("error", (err) => {
+            console.log(`An error occured ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+
+        })
+        .save(outputFilePath);
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video Processing service listening at http://localhost:${port}`)
 });
