566f7a407f4eec69c959474a63f98bccf0d5738b
bryantto08 <bryantto08@gmail.com>
2023-10-09 14:39:43 -0400

    Add Videos to Firestore

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
 import { OutgoingMessage } from "http";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories(); // creating local storage for raw and processed videoes
 
@@ -28,8 +29,19 @@ app.post('/process-video', async (req, res) => {
         return res.status(400).send("Bad Request: missing filename");
     }
 
-    const inputFileName = data.name;
+    const inputFileName = data.name;  // Format of <UID>-<DATE> , <EXTENSION
     const outputFilename = `processed-${inputFileName}`;
+    const videoId = inputFileName.split('.')[0];  // <UID>-<DATE>
+    
+    if (!isVideoNew(videoId)) {
+        return res.status(400).send("Bad Request: Video already processing or processed");
+    } else {
+        await setVideo(videoId, {
+            id: videoId,
+            uid: videoId.split("-")[0],
+            status: 'processing',
+        });
+    }
 
     // Download the raw video from Cloud Storage
     await downloadRawVideo(inputFileName);
@@ -48,6 +60,12 @@ app.post('/process-video', async (req, res) => {
 
     // Upload the processed video to Cloud storage
     await uploadProcessedVideo(outputFilename);
+
+    setVideo(videoId, {
+        status: 'processed',
+        filename: outputFilename
+    });
+
     await Promise.all([
         deleteRawVideo(inputFileName),
         deleteProcessedVideo(outputFilename)
